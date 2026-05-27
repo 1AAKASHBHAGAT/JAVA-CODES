00949ac098b3620155de1c357c462e769bae6580
aakashbhagat-tech <grab10aakashbhagat@gmail.com>
2026-05-27 23:16:43 +0530

    my first question using hash maps

---
+++ TwoSum.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+        int arr[] = {2, 7, 11, 15};
+        int target = 9;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < arr.length; i++) {
+
+            int complement = target - arr[i];
+
+            if(map.containsKey(complement)) {
+
+                System.out.println("Indices: "
+                        + map.get(complement)
+                        + " and " + i);
+
+                System.out.println("Numbers: "
+                        + complement
+                        + " and " + arr[i]);
+
+                return;
+            }
+
+            map.put(arr[i], i);
+        }
+
+        System.out.println("No two numbers found");
+    }
+}
