aa0508a2669e9819ba1dae2936c1a583f18bf76c
aakashbhagat-tech <grab10aakashbhagat@gmail.com>
2026-05-28 21:47:36 +0530

    just another banger day and practice makes a coder a software engineer

---
+++ FirstRepeatingElement.java
@@ -0,0 +1,22 @@
+public class FirstRepeatingElement {
+
+    public static void main(String[] args) {
+
+        int arr[] = {3,5,7,5,3,1};
+
+        for(int i = 0; i < arr.length; i++) {
+
+            for(int j = i + 1; j < arr.length; j++) {
+
+                if(arr[i] == arr[j]) {
+
+                    System.out.println("First Repeating Element = " + arr[i]);
+
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No Repeating Element");
+    }
+}
