05dc5e7e3fb3847d98b7e65d03b1f8f07e04748e
aakashbhagat-tech <grab10aakashbhagat@gmail.com>
2026-05-30 08:55:01 +0530

    practice in java

---
+++ RevisionDay1q1.java
@@ -0,0 +1,19 @@
+ import java.util.*; 
+public class RevisionDay1q1 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int xm1 = sc.nextInt();
+        int xm2 = sc.nextInt();
+        int xm3 = sc.nextInt();
+        int xm4 = sc.nextInt();
+        int xm5 = sc.nextInt();
+        
+    double avg ;
+      avg = (xm1 + xm2 + xm3 + xm4 + xm5)/5.0;
+        System.out.printf("%.2f" ,avg);
+    }
+   
+} 
+  
+
